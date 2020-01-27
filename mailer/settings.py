870612dedc3cb92bcac47b93b25ee0d0600a85df
Vladimir <scherbinin.vladimir@gmail.com>
2020-01-27 14:43:34 +0300

    deploy to heroku

--- mailer/settings.py
+++ mailer/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'mailer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
